a228c759d16d648084f893bb7c1b7fe8303a7a45
Mizan <ghodafail@gmail.com>
2025-11-08 16:40:32 +0700

    peta leaflet

--- app/layout.js
+++ app/layout.js
@@ -32,6 +32,7 @@ export default function RootLayout({ children }) {
           src="https://unpkg.com/leaflet@1.9.4/dist/leaflet.js"
           integrity="sha256-20nQCchB9co0qIjJZRGuk2/Z9VM+kNiyxNV1lvTlZBo="
           crossOrigin=""
+          strategy="beforeInteractive"
         />
       </body>
     </html>
