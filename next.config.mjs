fc7636bbd853ca6b4d6dd453f3a840086fa8f4f0
Mizan <ghodafail@gmail.com>
2025-11-08 16:09:17 +0700

    fixed

--- next.config.mjs
+++ next.config.mjs
@@ -1,13 +1,7 @@
-import { withPayload } from "@payloadcms/next/withPayload";
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  // Your Next.js config here
-  experimental: {
-    reactCompiler: false,
-  },
+  // Your Next.js config options go here.
+  // For a basic site, it can be empty.
 };
 
-// Make sure you wrap your `nextConfig`
-// with the `withPayload` plugin
-export default withPayload(nextConfig);
+export default nextConfig;
